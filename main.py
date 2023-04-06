10738d540141153ca800acf0c801a72657f36bd3
Karli Gyula Sámuel <samu.karli@limaeurope.com>
2023-04-06 17:06:05 +0200

    Merging files from home Added zip and empty drafter fields

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os.path
+
 from lxml import etree as et
 import xlrd
+import re
 
 _A_ =  0;   _B_ =  1;   _C_ =  2;   _D_ =  3;   _E_ =  4
 _F_ =  5;   _G_ =  6;   _H_ =  7;   _I_ =  8;   _J_ =  9
@@ -53,28 +56,41 @@ class SourceRow:
         self.row = self.sheet.row(p_iRow)
 
     def __getitem__(self, item):
-        return self.row[item]
+        if isinstance(val := self.row[item].value, str):
+            return val
+        elif isinstance(val, float):
+            return str(int(val))
+        else:
+            return str(val)
 
 
-def createXML(p_iRow="14", p_sXLSPath="C:\\Users\\gyula.karli\\OneDrive - LIMA Design Kft\\HU22-036-TE_Helyszin-Klaszterek.xls"):
+def createXML(p_iRow="14", p_sXLSPath=r"C:\Users\samu.karli\OneDrive - LIMA Design Kft\HU22-036-TE_Helyszin-Klaszterek.xls"):
     sourceRow = SourceRow(int(p_iRow), p_sXLSPath)
+
+    if sourceRow[_B_] not in {"ÉA", "ÉM"} or not re.match(r'\d+', str(sourceRow[_G_])):
+        return
 
     projectInfo = et.Element("ProjectInfo")
 
     ver = et.SubElement(projectInfo, "Version")
     ver.attrib["val"] = "3"
 
+    FixKey.iFixKeys = 0
+    CustomKey.iCustomKeys = 0
+
     fixKeys = [FixKey("Project Name", "PROJECTNAME", "<####>"),
                FixKey("Client Company", "CLIENTCOMPANY", ""),
-               FixKey("Site Name", "SITE_NAME", sourceRow[_H_].value),
-               FixKey("Site ID", "SITE_ID", sourceRow[_O_].value),
-               FixKey("Site Address1", "SITEADDRESS1", sourceRow[_K_].value),
-               FixKey("Site City", "SITECITY", sourceRow[_I_].value),
+               FixKey("Site Name", "SITE_NAME", sourceRow[_H_]),
+               FixKey("Site ID", "SITE_ID", sourceRow[_P_]),
+               FixKey("Site Address1", "SITEADDRESS1", sourceRow[_L_]),
+               FixKey("Site City", "SITECITY", sourceRow[_I_]),
+               FixKey("Site Postcode", "SITEPOSTCODE", sourceRow[_J_]),
                ]
 
-    customKeys = [CustomKey("CAD Drafter Responsible Name", "autotext-PROJECT-3F27A8D0-6FBE-4EF8-A3AA-8C0D4D61DDAE", sourceRow[_F_].value),
+    customKeys = [
+                  CustomKey("CAD Drafter Responsible Name", "autotext-PROJECT-3F27A8D0-6FBE-4EF8-A3AA-8C0D4D61DDAE", ""),
                   CustomKey("CAD Drafter - Company Name", "autotext-PROJECT-C179E160-3C5D-41A3-8125-1EEFD66B6BB7", "Lima Design Kft."),
-                  CustomKey("OM", "autotext-SITE-D1A8B19C-00D2-4D00-898E-C98BA30E26A8", sourceRow[_S_].value),
+                  CustomKey("OM", "autotext-SITE-D1A8B19C-00D2-4D00-898E-C98BA30E26A8", sourceRow[_Q_]),
                   CustomKey("Survey Company Name", "autotext-PROJECT-03B4AF22-C085-4499-9E7A-5E322E8489FE", "Lima Design Kft."),
                   CustomKey("Survey Responsible Name", "autotext-PROJECT-61034D80-84F0-4A5A-BA73-ADF24B043268", "Dormán Bertalan"),
                   ]
@@ -100,8 +116,11 @@ def createXML(p_iRow="14", p_sXLSPath="C:\\Users\\gyula.karli\\OneDrive - LIMA D
 
     tree = et.ElementTree(projectInfo)
     et.indent(tree, " ")
-    tree.write("output.xml", xml_declaration=True, encoding="UTF-8")
+    sOutFile = os.path.join("output", sourceRow[_B_] + "-" + str(int(sourceRow[_G_])) + ".xml")
+
+    tree.write(sOutFile, xml_declaration=True, encoding="UTF-8")
 
 if __name__ == "__main__":
-    # createXML(sys.argv[1], sys.argv[2])
-    createXML()
+    for i in range(6, 274):
+        createXML(str(i))
+
